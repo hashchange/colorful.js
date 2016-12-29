3db50a0172d187b75fe17a61a8665ae8c17ce039
hashchange <heim@zeilenwechsel.de>
2016-12-29 11:43:40 +0100

    Initial build

---
+++ demo/amd/rjs/output/parts/app.js
@@ -0,0 +1,17 @@
+// main.js
+
+require( [
+
+    'jquery',
+    'underscore',
+    'vue',
+    'colorful'
+
+], function ( $, _, Vue, Colorful ) {
+
+    var Color = Colorful.Color;
+
+} );
+
+define("local.main", function(){});
+
